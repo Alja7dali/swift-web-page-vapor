8100e4a99d276fc48d962aeaaf5129d9194ed264
Abdullah Aljahdali <Alja7dali@gmail.com>
2021-08-29 02:23:20 +0300

    👋 hello, swep-vapor!

--- Sources/SwepVaporSupportExample/AdvancedUsage.swift
+++ Sources/SwepVaporSupportExample/AdvancedUsage.swift
@@ -78,10 +78,10 @@ let advancedUsage = {
       }
     }
     """)
-    div {
-      p("Now, in your router you can wrap the content that is specific to that page in your layout by just invoking the ")
+    p {
+      text("Now, in your router you can wrap the content that is specific to that page in your layout by just invoking the ")
       code("layout(content:)")
-      p("function:")
+      text("function:")
     }
     pre("""
     import Swep
@@ -99,10 +99,10 @@ let advancedUsage = {
     }
     try app.run()
     """)
-    div {
-      p("Now a full HTML/CSS page will be rendered, including a ")
+    p {
+      text("Now a full HTML/CSS page will be rendered, including a ")
       code("head")
-      p(" tag with stylesheets, a header with navigation links, the main content in the middle, and a footer with a copyright notice. The best part is that because everything is just simple Swift data we are free to arrange and compose this data in anyway we want. In particular, we are allowed to keep all of our helper nodes in the same file if we want, or we can break them out into multiple files if that works better for us. Compare that to templates where you are forced to always put views in a new file, no matter how tiny and insignificant it is.")
+      text(" tag with stylesheets, a header with navigation links, the main content in the middle, and a footer with a copyright notice. The best part is that because everything is just simple Swift data we are free to arrange and compose this data in anyway we want. In particular, we are allowed to keep all of our helper nodes in the same file if we want, or we can break them out into multiple files if that works better for us. Compare that to templates where you are forced to always put views in a new file, no matter how tiny and insignificant it is.")
     }
   }
 }
